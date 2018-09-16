003459bfd8718906cb971838e176dd9ed88b792c
Gilvam Mourão <gilvamm@gmail.com>
2018-09-16 10:39:02 -0300

    Adicionado select que retorna um objeto junto ao uso do compareWith

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -18,6 +18,7 @@ import { Observable } from 'rxjs';
 export class DataFormComponent implements OnInit {
   formulario: FormGroup;
   estados: Observable<EstadoBr[]>;
+  cargos: any[];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -28,6 +29,8 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit() {
     this.estados = this.dropdownService.getEstadosBr();
+
+    this.cargos = this.dropdownService.getCargos();
 
     /*this.formulario = new FormGroup({
       nome: new FormControl(null),
@@ -50,7 +53,9 @@ export class DataFormComponent implements OnInit {
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
         estado: [null, Validators.required]
-      })
+      }),
+
+        cargo: [null]
     });
 
     // tslint:disable-next-line:max-line-length
@@ -150,5 +155,14 @@ export class DataFormComponent implements OnInit {
         estado: null
       }
     });
+  }
+
+  setarCargo() {
+      const cargo = {nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl'};
+      this.formulario.get('cargo').setValue(cargo);
+  }
+
+  compararCargos(obj1, obj2) {
+    return obj1 && obj2 ? (obj1.nome === obj2.nome && obj1.nivel === obj2.nivel) : obj1 && obj2;
   }
 }
